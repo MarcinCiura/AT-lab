0dcd6faad89be9bae5396b18a3641ca25bfa42d0
MarcinCiura <mciura@gmail.com>
2024-05-21 15:27:46 +0200

    Poprawki

--- 5/suffixarray/suffixarray.go
+++ 5/suffixarray/suffixarray.go
@@ -11,14 +11,14 @@ type suffix struct {
 }
 
 // sortedSuffixes zwraca wycinek, którego elementami są wszystkie
-// sufiksy łańcucha `data`. Jeśli pewien sufiks s1 poprzedza inny
-// sufiks s2 w porządku leksykograficznym, to indeks pozycji, od
-// której zaczyna się sufiks s1, występuje w zwracanym wycinku
-// przed indeksem pozycji, od której zaczyna się sufiks s2
-func sortedSuffixes(data []byte) []suffix {
-	suffixes := make([]suffix, len(data))
+// sufiksy łańcucha `text`. Jeśli pewien sufiks S1 poprzedza inny
+// sufiks S2 w porządku leksykograficznym, to indeks tej pozycji,
+// od której zaczyna się sufiks S1, występuje w zwracanym wycinku
+// przed indeksem tej pozycji, od której zaczyna się sufiks S2
+func sortedSuffixes(text []byte) []suffix {
+	suffixes := make([]suffix, len(text))
 	for i := range suffixes {
-		suffixes[i].s = data[i:]
+		suffixes[i].s = text[i:]
 		suffixes[i].n = i
 	}
 	sort.Slice(suffixes, func(i, j int) bool {
@@ -27,69 +27,39 @@ func sortedSuffixes(data []byte) []suffix {
 	return suffixes
 }
 
-// Index implementuje tablicę sufiksów
+// Index implementuje indeks tekstu `text`
 type Index struct {
-	data []byte
-	ints []int
+	suffixes []int
+	text     []byte
 }
 
-// New zwraca nowy Index dla łańcucha `data`
-func New(data []byte) Index {
-	suffixes := sortedSuffixes(data)
+// New zwraca tablicę sufiksów łańcucha `text`
+func New(text []byte) Index {
 	index := Index{
-		make([]byte, len(data)),
-		make([]int, len(data)),
+		make([]int, len(text)),
+		make([]byte, len(text)),
 	}
-	index.data = data
-	for i := range suffixes {
-		index.ints[i] = suffixes[i].n
+	index.text = text
+	for i, suf := range sortedSuffixes(text) {
+		index.suffixes[i] = suf.n
 	}
 	return index
 }
 
-// Suffix zwraca ten sufiks łańcucha indeksowanego przez
-// index `x`, który występuje na i-tej pozycji w tablicy
-// sufiksów zawartej w indeksie `x`
+// Suffix zwraca ten sufiks łańcucha `x.text`, który
+// zaczyna się od `i`-tej pozycji tego łańcucha
 func (x Index) Suffix(i int) []byte {
-	return x.data[i:]
+	return x.text[i:]
 }
 
 // LookupAll zwraca wycinek, którego elementami są wszystkie
-// te pozycje, na których łańcuch `s` występuje w łańcuchu
-// indeksowanym przez Index
+// te pozycje, na których łańcuch `s` występuje w łańcuchu `x.text`
 func (x Index) LookupAll(s []byte) []int {
-	i := sort.Search(len(x.ints), func(i int) bool {
-		return bytes.Compare(x.Suffix(x.ints[i]), s) >= 0
+	i := sort.Search(len(x.suffixes), func(i int) bool {
+		return bytes.Compare(x.Suffix(x.suffixes[i]), s) >= 0
 	})
-	j := i + sort.Search(len(x.ints)-i, func(j int) bool {
-		return !bytes.HasPrefix(x.Suffix(x.ints[i+j]), s)
+	j := i + sort.Search(len(x.suffixes)-i, func(j int) bool {
+		return !bytes.HasPrefix(x.Suffix(x.suffixes[i+j]), s)
 	})
-	return x.ints[i:j]
-}
-
-// lenOfCommonPrefix zwraca długość najdłuższego
-// wspólnego prefiksu łańcuchów `s` i `t`
-func lenOfCommonPrefix(s, t []byte) int {
-	k := 0
-	for ; k < min(len(s), len(t)); k++ {
-		if s[k] != t[k] {
-			return k
-		}
-	}
-	return k
-}
-
-// LongestRepeatingSubstring znajduje najdłuższy taki łańcuch,
-// który występuje w łańcuchu `text` co najmniej `k` razy
-func LongestRepeatingSubstring(text []byte, k int) []byte {
-	index := New(text)
-	substr := []byte{}
-	for i := 0; i+k <= len(text); i++ {
-		m := lenOfCommonPrefix(
-			index.Suffix(i), index.Suffix(i+k-1))
-		if m > len(substr) {
-			substr = index.Suffix(i)[:m]
-		}
-	}
-	return substr
+	return x.suffixes[i:j]
 }
